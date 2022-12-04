a8024145836cc218d1c29d93cb3e38d3a4ce7211
Parz1val1 <timmyhawkins3@gmail.com>
2022-12-03 20:26:04 -0500

    Add project files.

---
+++ AdventOfCode/Day1/Day1.cs
@@ -0,0 +1,25 @@
+﻿// read file
+var input = System.IO.File.ReadAllText(@"..\..\..\Day1\input.txt").Trim();
+Console.WriteLine("INPUT:\n" + input);
+
+// split into groups
+var numGroups = input.Split("\n\n");
+Console.WriteLine("NUM GROUPS:\n" + numGroups.Length);
+
+// get sum of groups
+var sumGroups = new List<int>();
+foreach (var group in numGroups)
+{
+    var calories = group.Split("\n");
+    var calorySum = 0;
+    foreach (var cal in calories)
+    {
+        calorySum += Int32.Parse(cal.Trim());
+    }
+    sumGroups.Add(calorySum);
+}
+
+// get max in collection
+var max = sumGroups.Max();
+Console.WriteLine("SUM GROUPS:\n" + max);
+return max;
